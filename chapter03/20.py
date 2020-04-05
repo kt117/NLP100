6a87ba0af43cf0b18a214225edfc8792272741d2
kt117 <divinediscon10t@gmail.com>
2020-04-06 07:09:35 +0900

    change: 保存形式を変更

--- chapter03/20.py
+++ chapter03/20.py
@@ -5,10 +5,9 @@ def solve(f):
     for line in f:
         article = json.loads(line[: -1])
         if article["title"] == "イギリス":
-            return article
+            return article["text"]
 
 
-with open("data/jawiki-country.json") as f, open("output/article.pickle", mode='wb') as g:
-    article = solve(f)
-    print(article)
-    pickle.dump(article, g)
+with open("data/jawiki-country.json") as f, open("output/england.pickle", mode='wb') as g:
+    text = solve(f)
+    pickle.dump(text, g)
