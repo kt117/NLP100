644d8c086186baa670b4d916684eb46ab198a69c
kt117 <divinediscon10t@gmail.com>
2020-04-14 23:41:26 +0900

    clene: rename etc

--- chapter04/30.py
+++ chapter04/30.py
@@ -1,25 +1,24 @@
-import sys
 import MeCab
 
 
 def solve(f):
     tagger = MeCab.Tagger("-d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd")
 
-    words = list()
+    morphemes = list()
     for line in f.readlines():
         node = tagger.parseToNode(line[: -1])
 
         while node:
             res = node.feature.split(",")
-            word = {"surface": node.surface, "base": res[6], "pos": res[0], "pos1": res[1]}
-            words.append(word)
+            morpheme = {"surface": node.surface, "base": res[6], "pos": res[0], "pos1": res[1]}
+            morphemes.append(morpheme)
             
             node = node.next
 
-    return words
+    return morphemes
 
 
 with open("data/neko.txt") as f:
-    words = solve(f)
-    for w in words[: 10]:
-        print(w)
+    morphemes = solve(f)
+    for m in morphemes[: 10]:
+        print(m)
