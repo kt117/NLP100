e7e17b6d7bd9cc429d19633576f7cd1b2ece9c0e
kt117 <k.tohyama17@gmail.com>
2020-06-06 23:23:01 +0900

    init: cabocha

---
+++ chapter05/40.py
@@ -0,0 +1,62 @@
+import CaboCha
+
+
+def read_file(f):
+    res = list()
+    for line in f.readlines():
+        res.append(line[: -1])
+    return res[2 :]
+
+
+def cabocha_parse(f, g):
+    sentences = read_file(f)
+
+    parser = CaboCha.Parser()
+    for sentence in sentences:
+        g.write(parser.parse(sentence).toString(CaboCha.FORMAT_LATTICE))
+
+
+with open("data/neko.txt") as f, open("output/neko.txt.cabocha", mode='w') as g:
+    cabocha_parse(f, g)
+    
+
+class Morph:
+    surface = ""
+    base = ""
+    pos = ""
+    pos1 = ""
+
+    def __init__(self, morpheme):
+        self.surface = morpheme["surface"]
+        self.base = morpheme["base"]
+        self.pos = morpheme["pos"]
+        self.pos1 = morpheme["pos1"]
+
+
+def solve(f):
+    morphemes_all = list()
+
+    morphemes = list()
+    for line in f.readlines():
+        if line[0] == '*':
+            continue
+
+        if line == "EOS\n":
+            morphemes_all.append(morphemes)
+            morphemes = list()
+        else:
+            morpheme = line[: -1].split('\t')
+            surface = morpheme[0]
+            morpheme = morpheme[1].split(',')
+            morphemes.append(Morph({"surface": surface, "base": morpheme[6], "pos": morpheme[0], "pos1": morpheme[1]}))
+
+    return morphemes_all
+
+
+with open("output/neko.txt.cabocha") as f:
+    morphemes_all = solve(f)
+
+    for morphemes in morphemes_all[: 10]:
+        for morpheme in morphemes:
+            print(morpheme.surface)
+        print("")
