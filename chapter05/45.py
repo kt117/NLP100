e680595ed0b677b3012c24be8460adbc6693013d
kt117 <k.tohyama17@gmail.com>
2020-07-29 17:24:46 +0900

    change; change verb_to_cases_list structure

--- chapter05/45.py
+++ chapter05/45.py
@@ -52,7 +52,7 @@ def create_chunks_list(f):
 
 
 def pos_in_chunk(pos, chunk):
-    return [str(morph) for morph in chunk.morphs if morph.pos == pos]
+    return [morph for morph in chunk.morphs if morph.pos == pos]
 
 
 def solve(f):
@@ -61,19 +61,26 @@ def solve(f):
     verb_to_cases_list = list()
 
     for chunks in chunks_all:
-        for chunk_a in chunks:
-            if chunk_a.dst != -1:
-                chunk_b = chunks[chunk_a.dst]
-                verbs = pos_in_chunk("動詞", chunk_b)
+        chunk_to_cases = [list() for _ in chunks] 
 
-                if len(verbs) > 0:
-                    cases = pos_in_chunk("助詞", chunk_a)
-                    verb_to_cases_list.append({"verb": verbs[0], "cases": cases})
+        for chunk in chunks:
+            if chunk.dst != -1:
+                chunk_to_cases[chunk.dst] += pos_in_chunk("助詞", chunk)
 
-    with open("output/45.txt", mode='w') as g:
-        for verb_to_cases in verb_to_cases_list:
-            g.write(verb_to_cases["verb"] + " " + ' '.join(verb_to_cases["cases"]) + "\n")
+        for i, cases in enumerate(chunk_to_cases):
+            verbs = pos_in_chunk("動詞", chunks[i])
+
+            if len(verbs) > 0 and len(cases) > 0:
+                verb_to_cases_list.append({"verb": verbs[0], "cases": cases})
+
+    return verb_to_cases_list
 
 
 with open("output/ai.ja.txt.parsed") as f:
-    solve(f)
+    verb_to_cases_list = solve(f)
+
+
+with open("output/45.txt", mode='w') as g:
+    for verb_to_cases in verb_to_cases_list:
+        case_surfaces = [case.surface for case in verb_to_cases["cases"]]
+        g.write(verb_to_cases["verb"].base + " " + ' '.join(case_surfaces) + "\n")
