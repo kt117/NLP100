d22b62670500ba1dca2f7dc93e37d1729a5989c5
kt117 <k.tohyama17@gmail.com>
2021-03-01 19:34:30 +0900

    clean: quote

--- chapter07/63.py
+++ chapter07/63.py
@@ -1,5 +1,5 @@
 from gensim.models import KeyedVectors
 
 
-model = KeyedVectors.load_word2vec_format('chapter07/data/GoogleNews-vectors-negative300.bin', binary=True)
+model = KeyedVectors.load_word2vec_format("chapter07/data/GoogleNews-vectors-negative300.bin", binary=True)
 print(model.most_similar(positive=["Spain", "Athens"], negative=["Madrid"]))
--- chapter07/65.py
+++ chapter07/65.py
@@ -3,12 +3,12 @@ from tqdm import tqdm
 
 
 with open("chapter07/outputs/64.txt") as f:
-    count = {'semantic': [0, 0], 'syntactic': [0, 0]}
+    count = {"semantic": [0, 0], "syntactic": [0, 0]}
     current_task = None
     for line in tqdm(f.readlines()):
         words = line[: -1].split()
-        if words[0] == ':':
-            current_task = 'syntactic' if "gram" == words[1][: 4] else 'semantic'
+        if words[0] == ":":
+            current_task = "syntactic" if "gram" == words[1][: 4] else "semantic"
         else:
             count[current_task][0] += 1  
             if words[3] == words[4]:
